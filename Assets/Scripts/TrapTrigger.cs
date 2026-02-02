a572df6f5789c5b375f549895249229b9c37fe0a
Jaeden Glace <jsg155@case.edu>
2026-02-02 00:30:10 -0500

    Trap Trigger and Action Draft
    Created draft for two scripts that take care of traps. One attaches to the object that is triggered, and the other to the object that changes as a result of the action.

---
+++ Assets/Scripts/TrapTrigger.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TrapTrigger : MonoBehaviour
+{
+    [Header("Detection Settings")]
+    [SerializeField] private string targetTag = "Player";
+    [SerializeField] private bool triggerOnlyOnce = false;
+
+    [Header("The Action")]
+    // This creates a slot in the Inspector to drag-and-drop ANY functionality
+    public UnityEvent onTrapActivated;
+
+    private bool hasTriggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (hasTriggered) return;
+
+        if (other.CompareTag(targetTag))
+        {
+            ActivateTrap();
+        }
+    }
+
+    private void ActivateTrap()
+    {
+        Debug.Log($"Trap on {gameObject.name} activated!");
+
+        // This runs whatever functions you've added in the Inspector
+        onTrapActivated?.Invoke();
+
+        if (triggerOnlyOnce)
+        {
+            hasTriggered = true;
+        }
+    }
+}
